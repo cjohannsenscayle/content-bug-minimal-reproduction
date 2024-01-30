8e1276975eb364903bc5100e87ee00feb6543b74
Christoph Johannsen <christoph.johannsen@scayle.com>
2024-01-30 14:56:15 +0100

    add test code for reproduction

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,20 @@
 <template>
   <main>
-    <ContentDoc />
+    <div>Loading content from content/.gitbook</div>
+    <div v-if="data">data: {{ data }}</div>
+    <div v-if="error">error: {{ error }}</div>
   </main>
 </template>
+
+<script setup lang="ts">
+const FILE_PATH = ".gitbook/test";
+
+let data: any;
+let error: any;
+
+try {
+  data = await queryContent(FILE_PATH).findOne();
+} catch (e) {
+  error = e;
+}
+</script>
